a47b0fa4acd3b91bba116038c17fddb7d10c1a7d
Leanderson Moraes <leandersonmoraes12@gmail.com>
2025-10-29 20:57:01 -0300

    Finalizando dicionário

--- dicionario_agenda.py
+++ dicionario_agenda.py
@@ -4,15 +4,20 @@ while True:
     print("\nAgenda Eletrônica\n")
     print("1- Inserir dados na agenda")
     print("2- Excluir dados da agenda")
-    print("3- Sair do sistema")
+    print("3- Consultar todos os dados da agenda")
+    print("4- Sair do sistema")
     resposta = int(input("Qual sua escolha: "))
 
     if resposta == 1:
-        nome = input("Qual o nome do contato: ")
-        agenda[nome] = int(input(f"Qual o número de {nome}: "))
-        
-        print("Contato inserido com sucesso!")
-        print(agenda)
+        try:
+            nome = input("Qual o nome do contato: ")
+            agenda[nome] = int(input(f"Qual o número de {nome}: "))
+            
+            print("Contato inserido com sucesso!")
+            print(agenda)
+            
+        except Exception:
+            print("Operação inválida. Contato não salvo.")
 
     elif resposta == 2:
         print(agenda)
@@ -26,9 +31,13 @@ while True:
 
         else:
             print(f"O contato {escolha} não existe na agenda")
+            
+    elif resposta == 3:
+        for chave, valor in agenda.items():
+            print(f"{chave} : {valor}")
         
 
 
-    elif resposta == 3:
+    elif resposta == 4:
         print("Sistema Encerrado")
         break
---
+++ tratamento_erro.py
@@ -0,0 +1,6 @@
+try:
+    valor = int(input("Informe um valor númerico: "))
+    print(f"O número digitado foi {valor}")
+      
+except Exception as erro:
+    print(f"Coisa feia, tentando colocar texto no lugar de números. O erro foi: {erro}")
